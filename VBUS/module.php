75c7addb35b4efa1a7827e7246ee60425d38609c
IPSAttain <christianwagner1@yahoo.de>
2020-07-29 11:04:33 +0200

    Übernahme aus Script

--- VBUS/module.php
+++ VBUS/module.php
@@ -30,6 +30,57 @@
 		{
 			$data = json_decode($JSONString);
 			IPS_LogMessage("Device RECV", utf8_decode($data->Buffer));
+			$debug = true;
+			$value = utf8_decode($data->Buffer);
+			define('ANZAHL_FRAMES', ord($value{6}));
+			define('HEADER_CHECKSUMME', ord($value{7}));
+			define('REGLER_TYP', "0x" . dechex(ord($value{2})) . dechex(ord($value{1} )));
+			define('SCRIPT_KENNUNG', 'V-Bus-Script ID : ' . $_IPS['SELF']);
+			define('XML_DATEI', 'VBusSpecificationResol.xml');
+			if ($debug) IPS_LogMessage(SCRIPT_KENNUNG,REGLER_TYP);
+
+			$cs = 16;       // durch den Cutter wird das erste Byte (0x10 Hex) abgeschnitten, hier wird der Wert wieder dazu genommen
+			for ($i=00; $i<=06; $i++)
+			{
+				$cs += ord($value{$i}); //Headerbytes zur Checksumme zusammenaddieren
+			}
+			$cs = ~$cs;	//Checksumme invertieren
+			$cs &= 127;	//MSB aus Checksumme entfernen
+			if ($debug) IPS_LogMessage(SCRIPT_KENNUNG,"Berrechnete Checksumme Header: $cs , Empfangene Checksumme: " . HEADER_CHECKSUMME);
+			if ( $cs == HEADER_CHECKSUMME)  // Checksumme ok?
+			{
+				if ($debug) IPS_LogMessage(SCRIPT_KENNUNG,"HeaderChecksumme OK!");
+				$byte_array = array();
+				$k = 0; // array Index
+				if ($debug) IPS_LogMessage(SCRIPT_KENNUNG,"Anzahl der ermittelten Frames: " . ANZAHL_FRAMES);
+				for ($i=01; $i<=ANZAHL_FRAMES; $i++) // Schleife für alle Datenframes
+				{
+				   $cs = 0;
+				   $septet = ord($value{$i * 6 + 6});
+					for ($j=00; $j<=03; $j++)
+					{  // es sind immer 4 Bytes in einem Frame
+						$payload_byte = ord($value{$i * 6 + 2 + $j});
+						$byte_array[$k] = $payload_byte + 128 * (($septet >> $j) & 1); //das komplette Datenbyte aus dem Byte und dem Teil des Septet zusammenfügen
+						$k++; //Array Index erhöhen
+						$cs += $payload_byte;// Bytes zur Checksumme addieren
+					} // End payload Byte Schleife
+					$cs += $septet; // septet dazuaddieren
+					$cs = ~$cs; //Checksumme invertieren
+					$cs &= 127; //MSB aus Checksumme entfernen
+					if ($debug) IPS_LogMessage(SCRIPT_KENNUNG,"Berrechnete Checksumme Frame $i: $cs , Empfangene Checksumme: ".ord($value{$i * 6 + 7}));
+					if ($cs != ord($value{$i * 6 + 7})) // Checksumme Frame not ok?
+					{
+						IPS_LogMessage(SCRIPT_KENNUNG,"Checksummenfehler im Frame $i >> ermittelte Summe: $cs empfangene Summe: ".ord($value{$i * 6 + 7}));
+						return;
+					}
+				} // end for frameschleife
+			}
+			else  // Checksumme Head not ok
+			{
+				IPS_LogMessage(SCRIPT_KENNUNG,"Checksummenfehler Header >>Checksumme berrechnet: $cs Checksumme soll: ".ord($value{7}));
+			}	// end else
+			if ($debug) IPS_LogMessage(SCRIPT_KENNUNG,print_r($byte_array));
+				
 		}
 
 	}
