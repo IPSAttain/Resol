ad760cc9cdc947e80ddd979f25768ad3ba9af416
IPSAttain <christianwagner1@yahoo.de>
2020-07-29 23:41:26 +0200

    First Release V1.0

--- VBUS/module.php
+++ VBUS/module.php
@@ -49,12 +49,15 @@
 
 		public function SendPass()
 		{
-			$data =  "PASS " . $this->ReadPropertyString("Password") . CHR(13);
-			$data .= "DATA" . CHR(13);
-			$this->SendDataToParent(json_encode([
-				'DataID' => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}",
-				'Buffer' => utf8_encode($data),
-			]));
+			if($this->HasActiveParent())
+			{
+				$data =  "PASS " . $this->ReadPropertyString("Password") . CHR(13);
+				$data .= "DATA" . CHR(13);
+				$this->SendDataToParent(json_encode([
+					'DataID' => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}",
+					'Buffer' => utf8_encode($data),
+				]));
+			}
 		}
 
 		public function ReceiveData($JSONString)
