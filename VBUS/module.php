837bcc50dfe11a20d2c24ccb3af9cdd91b7d8acf
IPSAttain <christianwagner1@yahoo.de>
2020-07-30 19:57:20 +0200

    bugfix

--- VBUS/module.php
+++ VBUS/module.php
@@ -89,7 +89,7 @@
 					$byte_array = array();
 					$k = 0; // array Index
 					$this->SendDebug("Frame Count","Number of Frames: " . (NUMBER_OF_FRAMES),0);
-					if (strlen($payload) < (NUMBER_OF_FRAMES * 6 + 11) 
+					if (strlen($payload) < (NUMBER_OF_FRAMES * 6 + 11)) 
 					{
 						$this->SendDebug("Payload","Lenght of Payload is to short. Calculated: " . (NUMBER_OF_FRAMES * 6 + 10) . "Received: ". strlen($payload),0);
 						return;
