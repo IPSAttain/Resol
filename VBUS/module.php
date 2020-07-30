0cd2fc58f3303c9ecc146ace6cd2b704ad6a10ab
IPSAttain <christianwagner1@yahoo.de>
2020-07-30 20:06:03 +0200

    intercept to short string lenght

--- VBUS/module.php
+++ VBUS/module.php
@@ -66,7 +66,7 @@
 			$language = $this->ReadPropertyInteger("VarName");
 			$this->SendDebug("Received", utf8_decode($data->Buffer) , 1);
 			$payload = utf8_decode($data->Buffer);
-			if (substr($payload,0,2) == "\xaa\x10" && strlen($payload) >= 16) 
+			if (substr($payload,0,2) == "\xaa\x10" && strlen($payload) >= 16) // it must have at least the header and one dataframe (16 bytes)
 			{
 				$value = ltrim(utf8_decode($data->Buffer), "\xaa\x10"); // remove the first 2 bytes, like the cutter
 				define('NUMBER_OF_FRAMES', ord($value{6}));
@@ -89,9 +89,9 @@
 					$byte_array = array();
 					$k = 0; // array Index
 					$this->SendDebug("Frame Count","Number of Frames: " . (NUMBER_OF_FRAMES),0);
-					if (strlen($payload) < (NUMBER_OF_FRAMES * 6 + 11)) 
+					if (strlen($payload) < (NUMBER_OF_FRAMES * 6 + 10)) // the length of the complete string must have the header (10 byte) and 6 byte for every data frame
 					{
-						$this->SendDebug("Payload","Lenght of Payload is to short. Calculated: " . (NUMBER_OF_FRAMES * 6 + 10) . "Received: ". strlen($payload),0);
+						$this->SendDebug("Payload","Lenght of Payload is to short. Calculated: " . (NUMBER_OF_FRAMES * 6 + 10) . "  Received: ". strlen($payload),0);
 						return;
 					}
 					for ($i=01; $i<=NUMBER_OF_FRAMES; $i++) // loop for all frames
