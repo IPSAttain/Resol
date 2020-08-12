4eae2c87301eb5df1359298fb3bc79fe1715eee5
IPSAttain <christianwagner1@yahoo.de>
2020-08-12 20:17:40 +0200

    Buffer 3

--- VBUS/module.php
+++ VBUS/module.php
@@ -78,9 +78,9 @@
 				$this->SendDebug("SerchPos", "AA10: " . $AA10pos . " AA00: " . $AA00pos, 0);
 				if ($AA10pos !== false && $AA00pos !== false && $AA10pos < $AA00pos)
 				{
-					$payload = substr($payload,$AA10pos,$AA00pos-$AA10pos); // cut from AA10 to the next AA
 					$this->SetBuffer("IncommingBuffer",substr($payload,$AA00pos));
 					$this->SendDebug("Buffer", substr($payload,$AA00pos), 0);
+					$payload = substr($payload,$AA10pos,$AA00pos-$AA10pos); // cut from AA10 to the next AA
 					$this->SendDebug("To Proceed", $payload, 1);
 					$this->ProccedData($payload);
 				} elseif ($AA10pos !== false && $AA00pos !== false && $AA10pos > $AA00pos)
