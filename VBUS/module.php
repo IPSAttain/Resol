5c968888759d35d4d7a40bdd3ef16fbfa383224c
IPSAttain <christianwagner1@yahoo.de>
2020-08-17 13:26:45 +0200

    remove "return" command

--- VBUS/module.php
+++ VBUS/module.php
@@ -60,24 +60,26 @@
 				$this->RecieveLANCommands(utf8_decode($data->Buffer));
 			} else 
 			{
-				if (!$this->ReadAttributeBoolean("PassTrueBit")) return;
-				$this->SendDebug("Received", utf8_decode($data->Buffer) , 1);
-				$payload = $this->GetBuffer("IncommingBuffer") . utf8_decode($data->Buffer);
-				$AA10pos = strpos($payload, "\xaa\x10\x00");
-				$AApos = strpos($payload, "\xaa",$AA10pos +1);
-				$this->SendDebug("SerchPos", "AA10: " . $AA10pos . " AA: " . $AApos, 0);
-				if ($AA10pos !== false && $AApos !== false)
+				if ($this->ReadAttributeBoolean("PassTrueBit"))
 				{
-					// found cutter values
-					$this->SetBuffer("IncommingBuffer",substr($payload,$AApos)); // put the rest back to the buffer
-					$this->SendDebug("Buffer", substr($payload,$AApos), 1);
-					$payload = substr($payload,$AA10pos,$AApos-$AA10pos); // cut from AA 10 00 to the next AA
-					$this->SendDebug("To Proceed", $payload, 1);
-					$this->ProccedData($payload);
-				} else
-				{
-					$this->SetBuffer("IncommingBuffer",$payload);
-					$this->SendDebug("Buffer", $payload, 1);
+					$this->SendDebug("Received", utf8_decode($data->Buffer) , 1);
+					$payload = $this->GetBuffer("IncommingBuffer") . utf8_decode($data->Buffer);
+					$AA10pos = strpos($payload, "\xaa\x10\x00");
+					$AApos = strpos($payload, "\xaa",$AA10pos +1);
+					$this->SendDebug("SerchPos", "AA10: " . $AA10pos . " AA: " . $AApos, 0);
+					if ($AA10pos !== false && $AApos !== false)
+					{
+						// found cutter values
+						$this->SetBuffer("IncommingBuffer",substr($payload,$AApos)); // put the rest back to the buffer
+						$this->SendDebug("Buffer", substr($payload,$AApos), 1);
+						$payload = substr($payload,$AA10pos,$AApos-$AA10pos); // cut from AA 10 00 to the next AA
+						$this->SendDebug("To Proceed", $payload, 1);
+						$this->ProccedData($payload);
+					} else
+					{
+						$this->SetBuffer("IncommingBuffer",$payload);
+						$this->SendDebug("Buffer", $payload, 1);
+					}
 				}
 			}
 		}
