dd8628f7beb551018e393f27ea9086a23c28fee3
IPSAttain <christianwagner1@yahoo.de>
2020-09-28 21:11:17 +0200

    Update 1

--- VBUS/module.php
+++ VBUS/module.php
@@ -202,7 +202,7 @@
 							{
 								$field_name = (string)@($field->name[0]); 
 							}
-							if (isset($field['commonUsage'][0]) $field_info = (string)@$field['commonUsage'][0];
+							//if (isset($field['commonUsage'][0]) $field_info = (string)@$field['commonUsage'][0];
 							if (isset($field->unit) $field_unit = (string)@$field->unit;
 							if (isset($field->bitSize) $field_bit_size = (int)@$field->bitSize;
 							$var_profil = "";
